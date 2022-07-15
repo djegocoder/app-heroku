904884e82bfb400006dbc8cfe87c2e9c60e54fea
diego tadeu <diegotadsil@gmail.com>
2022-07-14 21:54:28 -0300

    Uso do bootstrap

--- bet.py
+++ bet.py
@@ -1,13 +1,13 @@
 from crypt import methods
 import os
-from flask import Flask, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
-def inicio():
-    return "Heroku app Bet"
+@app.route("/")
+def index():
+    return render_template("layout.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
