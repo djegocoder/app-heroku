f232a086005155e2bc70509eb0867cf6070cc659
diego tadeu <diegotadsil@gmail.com>
2022-07-23 18:56:13 -0300

    Primeiro dashbord

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#from dash import Dash, html, dcc
+
 import plotly.express as px
 import plotly
 import json
@@ -9,12 +9,35 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 df = pd.read_csv('static/planilha.csv')
-fig = px.bar(df, x="Times", y="Sigla", color="Bet", barmode="group")
-graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+fig1 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",
+        color="Times", size=[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],opacity=0.5,symbol="Estádio",center={"lat":-15,"lon":-50})
+graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
+
+fig2 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",
+        color="Times", size=[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],opacity=0.5,symbol="Estádio",center={"lat":-15,"lon":-50})
+graph2JSON = json.dumps(fig2, cls=plotly.utils.PlotlyJSONEncoder)
+
+fig3 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",
+        color="Times", size=[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],opacity=0.5,symbol="Estádio",center={"lat":-15,"lon":-50})
+graph3JSON = json.dumps(fig3, cls=plotly.utils.PlotlyJSONEncoder)
+
+fig4 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",
+        color="Times", size=[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],opacity=0.5,symbol="Estádio",center={"lat":-15,"lon":-50})
+graph4JSON = json.dumps(fig4, cls=plotly.utils.PlotlyJSONEncoder)
+
+fig5 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",
+        color="Times", size=[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],opacity=0.5,symbol="Estádio",center={"lat":-15,"lon":-50})
+graph5JSON = json.dumps(fig5, cls=plotly.utils.PlotlyJSONEncoder)
+
+fig6 = px.scatter_geo(df,lon='longitude_decimal',lat="latitude_decimal",hover_name="Times", scope="south america",
+        color="Times", size=[15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15,15],opacity=0.5,symbol="Estádio",center={"lat":-15,"lon":-50})
+graph6JSON = json.dumps(fig6, cls=plotly.utils.PlotlyJSONEncoder)
+
 
 @app.route("/")
 def index():
-    return render_template("layout.html",graphJSON=graphJSON)
+    return render_template("layout.html",graph1JSON=graph1JSON,graph2JSON=graph2JSON,graph3JSON=graph3JSON,graph4JSON=graph4JSON,graph5JSON=graph5JSON,graph6JSON=graph6JSON)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
