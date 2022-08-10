7728e9046d84bb11e6f201eac331a934f56aa937
diego tadeu <diegotadsil@gmail.com>
2022-08-09 22:36:23 -0300

    mais portifolio

--- app.py
+++ app.py
@@ -60,9 +60,40 @@ def resume():
     return render_template("index.html")
 
 @app.route("/portifolio1")
-def portifolio():
-    return render_template("portfolio-details.html")
+def portifolio1():
+    return render_template("portifolio-1.html")
 
+@app.route("/portifolio2")
+def portifolio2():
+    return render_template("portifolio-2.html")
+
+@app.route("/portifolio3")
+def portifolio3():
+    return render_template("portifolio-3.html")
+
+@app.route("/portifolio4")
+def portifolio4():
+    return render_template("portifolio-4.html")
+
+@app.route("/portifolio5")
+def portifolio5():
+    return render_template("portifolio-5.html")
+
+@app.route("/portifolio6")
+def portifolio6():
+    return render_template("portifolio-6.html")
+
+@app.route("/portifolio7")
+def portifolio7():
+    return render_template("portifolio-7.html")    
+
+@app.route("/portifolio8")
+def portifolio8():
+    return render_template("portifolio-8.html")
+
+@app.route("/portifolio9")
+def portifolio9():
+    return render_template("portifolio-9.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT",5000))
